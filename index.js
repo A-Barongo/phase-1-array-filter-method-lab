1f88cba392c512217605ed9bf3ee3ad1c14b83a6
Allan <allanbarongo5@gmail.com>
2025-03-15 23:07:44 +0300

    Completed lab on array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driverNames,keyword){
+    let match=driverNames.filter(name=>name.toLowerCase()==keyword.toLowerCase())
+    return match
+}
+
+function fuzzyMatch(driverNames,query){
+    return driverNames.filter(driver=>driver.toLowerCase().startsWith(query.toLowerCase()))
+}
+
+function matchName(driverNames,keyword){
+    return driverNames.filter(driver=>driver.name===keyword)
+}
